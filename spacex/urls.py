25703186eb647b65f380a4740a4c4e8ae7e33a99
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-05 12:51:45 +0000

    Created urls for main_pages and included them in spacex.urls

--- spacex/urls.py
+++ spacex/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from main_pages.views import home_page, about_page
+from django.urls import path, include
+from main_pages.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page),
-    path('about/', about_page),
+
+    path('', home_page, name='home'),
+    path('main/', include('main_pages.urls')),
 ]
