d944bdffeeded0b2865a992945257070f41a8d9d
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-06 18:07:45 +0000

    Removed double quote for name pages

--- main_pages/urls.py
+++ main_pages/urls.py
@@ -3,5 +3,5 @@ from .views import about_page, contact_page
 
 urlpatterns = [
     path('about/', about_page, name='about'),
-    path('contact/', contact_page, name="contact"),
+    path('contact/', contact_page, name='contact'),
 ]
