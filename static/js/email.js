f9cd3b4fe2728e913461fc480c9d8bdd76dee571
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-06 11:27:28 +0000

    Passed user id for emailJS API as variable

---
+++ static/js/email.js
@@ -0,0 +1,21 @@
+(function () {
+    emailjs.init(emailjsUser);
+})();
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "spacex", {
+        "from_name": contactForm.first.value + " " + contactForm.last.value,
+        "from_email": contactForm.email.value,
+        "contact_request": contactForm.message.value,
+        "subject": contactForm.subject.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
