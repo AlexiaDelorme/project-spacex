4c4ab09041a7c126159e1c5038560620740f349b
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-09 18:48:29 +0000

    Added urls testing for main_pages

--- main_pages/tests/tests_urls.py
+++ main_pages/tests/tests_urls.py
@@ -1,1 +1,18 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from main_pages.views import home_page, contact_page, about_page
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_home_url_is_resolved(self):
+        url = reverse('home')
+        self.assertEquals(resolve(url).func, home_page)
+
+    def test_about_url_is_resolved(self):
+        url = reverse('about')
+        self.assertEquals(resolve(url).func, about_page)
+
+    def test_contact_url_is_resolved(self):
+        url = reverse('contact')
+        self.assertEquals(resolve(url).func, contact_page)
