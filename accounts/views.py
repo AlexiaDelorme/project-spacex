97a2d8db02d09575f6bb58f185bf90873602ee0b
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-10 15:54:06 +0000

    Wired up login authentication

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,21 @@ def logout_page(request):
 
 
 def login_page(request):
-    login_form = UserLoginForm()
-    return render(request, "login.html", {'form': login_form})
 
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+            else:
+                login_form.add_error(
+                    None, "Your username or password is incorrect")
+
+    else:
+        login_form = UserLoginForm()
+    return render(request, "login.html", {'form': login_form})
