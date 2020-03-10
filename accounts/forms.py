af9ff44b6155bdbbc8632cc96074179ea5746914
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-10 19:02:25 +0000

    Created sign up form and set email as log in method

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,20 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField()
+    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserSignupForm(UserCreationForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username',
+                  'email', 'password1', 'password2']
