a854b690ed0c0501c6bd7b30e72568a8388a5132
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-12 18:23:41 +0000

    Added settings for gmail password reset services

--- spacex/settings.py
+++ spacex/settings.py
@@ -136,3 +136,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAILJS_USER = os.environ.get('EMAILJS_USER')
 
 GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
