9732507af2aa51f8a206f677108e5342db069d4e
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-12 18:23:09 +0000

    Included password-reset routes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .views import logout_page, login_page, signup_page, profile_page
+
 
 urlpatterns = [
     path('logout/', logout_page, name='logout'),
     path('login/', login_page, name='login'),
     path('signup/', signup_page, name='signup'),
     path('profile/', profile_page, name='profile'),
+    path('password-reset/', include('accounts.urls_reset'))
 ]
