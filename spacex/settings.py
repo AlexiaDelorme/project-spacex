af7ca46ca30229c087765489a23d7c5f4cdcf859
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-13 20:58:53 +0000

    Attempt 2 to fix Travis build failes

--- spacex/settings.py
+++ spacex/settings.py
@@ -89,16 +89,18 @@ WSGI_APPLICATION = 'spacex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if development == True:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+# if development == True:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+"""
 else:
     DATABASES = {'default': dj_database_url.parse(
         os.environ.get('DATABASE_URL'))}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
