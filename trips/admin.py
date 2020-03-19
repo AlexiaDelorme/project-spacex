d78d084c4363e04162299cd6c478fe62d80335c6
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-19 21:08:03 +0000

    Created models for Trips

--- trips/admin.py
+++ trips/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Trip, RequiredDocument, DepartureSite
 
 # Register your models here.
+admin.site.register(Trip)
+
+admin.site.register(DepartureSite)
+
+admin.site.register(RequiredDocument)
--- trips/models.py
+++ trips/models.py
@@ -3,4 +3,35 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class DepartureSite(models.Model):
+    site = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.site
+
+
+class RequiredDocument(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Trip(models.Model):
+    title = models.CharField(max_length=50)
+    destination = models.CharField(max_length=30)
+    duration = models.DurationField()
+    price = models.IntegerField()
+    departure_date = models.DateField()
+    departure_site = models.ForeignKey(
+        DepartureSite, on_delete=models.PROTECT)
+    slot = models.IntegerField()
+    description = models.TextField()
+    distance = models.IntegerField()
+    required_document = models.ForeignKey(
+        RequiredDocument, on_delete=models.PROTECT)
+    img = models.FileField(blank=True)
+
+    def __str__(self):
+        return self.title
