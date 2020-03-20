240546461591a5a783470e472c12d0bb35171f35
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-20 18:45:43 +0000

    Create view for all-trips listing

--- trips/views.py
+++ trips/views.py
@@ -2,7 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Trip
 
 
 def faq_page(request):
     return render(request, "faq.html", {"page_title": "FAQs"})
+
+
+def trips_page(request):
+    trips = Trip.objects.all()
+    context = {
+        "page_title": "All Trips",
+        "trips": trips
+    }
+    return render(request, "trips.html", context)
