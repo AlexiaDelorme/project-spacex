f18c807c11125f3bd43ea0382a55c9c8b07c2356
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-20 18:46:08 +0000

    Created url for trips listing

--- trips/urls.py
+++ trips/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import faq_page
+from .views import faq_page, trips_page
 
 urlpatterns = [
     path('faq/', faq_page, name='faq'),
+    path('all-trips/', trips_page, name='all-trips'),
 ]
