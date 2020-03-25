3cb006c4b354aa7f04506739624f6b33f818e55c
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-25 18:59:58 +0000

    Created ContactDetails model for user

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import ContactDetail
 
 # Register your models here.
+admin.site.register(ContactDetail)
--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ContactDetail(models.Model):
+   
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, blank=False)
+    birth_date = models.DateField()
+    street_address1 = models.CharField(max_length=50, blank=False)
+    street_address2 = models.CharField(max_length=50, blank=True, null=True)
+    state = models.CharField(max_length=40, null=True)
+    postcode = models.CharField(max_length=20, blank=True, null=True)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    profile_pic = models.ImageField(blank=True, upload_to='profile_pics')
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
