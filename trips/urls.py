bbaa6496b04055f34b5c5988db22e891cbbb8bc4
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-27 15:27:28 +0000

    Renamed all_trips url name for all trips view

--- trips/urls.py
+++ trips/urls.py
@@ -3,6 +3,6 @@ from .views import faq_page, trips_page, trip_detail_page
 
 urlpatterns = [
     path('faq/', faq_page, name='faq'),
-    path('all-trips/', trips_page, name='all-trips'),
-    path('trip-detail/<int:pk>/', trip_detail_page, name='trip-detail'),
+    path('all-trips/', trips_page, name='all_trips'),
+    path('trip-detail/<int:pk>/', trip_detail_page, name='trip_detail'),
 ]
