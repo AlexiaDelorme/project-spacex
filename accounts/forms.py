8a8a22ffb0b3c9b6509e20bdbb0af045e311fd45
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-30 16:27:41 +0000

    Created for for user contact details

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
+from .models import ContactDetail, Passenger
 
 
 class UserLoginForm(forms.Form):
@@ -18,3 +19,18 @@ class UserSignupForm(UserCreationForm):
         model = User
         fields = ['first_name', 'last_name', 'username',
                   'email', 'password1', 'password2']
+
+class UserContactDetailForm(forms.ModelForm):
+    class Meta:
+        model = ContactDetail
+        fields = [
+            'phone_number',
+            'birth_date',
+            'street_address1',
+            'street_address2',
+            'state',
+            'postcode',
+            'town_or_city',
+            'country',
+            'profile_pic'
+            ]
