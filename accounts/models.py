fd78b3a3ecc2f24235f21c27f592e5c9a9375ab2
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-30 17:44:12 +0000

    Made all fields of ContactDetail model optional

--- accounts/models.py
+++ accounts/models.py
@@ -8,14 +8,14 @@ from django.contrib.auth.models import User
 class ContactDetail(models.Model):
    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=20, blank=False)
-    birth_date = models.DateField()
-    street_address1 = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    street_address1 = models.CharField(max_length=50, blank=True, null=True)
     street_address2 = models.CharField(max_length=50, blank=True, null=True)
-    state = models.CharField(max_length=40, null=True)
+    state = models.CharField(max_length=40, blank=True, null=True)
     postcode = models.CharField(max_length=20, blank=True, null=True)
-    town_or_city = models.CharField(max_length=40, blank=False)
-    country = models.CharField(max_length=40, blank=False)
+    town_or_city = models.CharField(max_length=40, blank=True, null=True)
+    country = models.CharField(max_length=40, blank=True, null=True)
     profile_pic = models.ImageField(blank=True, upload_to='profile_pics')
 
     def __str__(self):
