31c6742bb54733a38f6cbc96f7a68cad31aba113
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-30 16:27:17 +0000

    Registered passenger model to admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import ContactDetail
+from .models import ContactDetail, Passenger
 
 # Register your models here.
 admin.site.register(ContactDetail)
+admin.site.register(Passenger)
