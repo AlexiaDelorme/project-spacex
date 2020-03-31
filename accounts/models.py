6611b9786131371354805153934e47a46d53c7b7
Alexia Delorme <alexia.delorme@gmail.com>
2020-03-31 15:23:16 +0000

    Amended ContactDetail model to include passenger fields

--- accounts/models.py
+++ accounts/models.py
@@ -8,15 +8,18 @@ from django.contrib.auth.models import User
 class ContactDetail(models.Model):
    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Mandatory fields to travel
+    birth_date = models.DateField(blank=False)
+    citizenship = models.CharField(max_length=30, blank=False)
+    passport_id = models.CharField(max_length=30, blank=False)
+    # Optional fields
     phone_number = models.CharField(max_length=20, blank=True, null=True)
-    birth_date = models.DateField(blank=True, null=True)
     street_address1 = models.CharField(max_length=50, blank=True, null=True)
     street_address2 = models.CharField(max_length=50, blank=True, null=True)
     state = models.CharField(max_length=40, blank=True, null=True)
     postcode = models.CharField(max_length=20, blank=True, null=True)
     town_or_city = models.CharField(max_length=40, blank=True, null=True)
     country = models.CharField(max_length=40, blank=True, null=True)
-    profile_pic = models.ImageField(blank=True, upload_to='profile_pics')
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
