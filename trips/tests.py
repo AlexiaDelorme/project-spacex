64e12695944cec23c405a7f29fa2b52c73c421d1
Alexia Delorme <alexia.delorme@gmail.com>
2020-04-02 18:42:34 +0000

    Set up test for trips

--- trips/tests.py
+++ trips/tests.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from .models import Trip
 
 # Create your tests here.
+class TripTests(TestCase):
+       
+    def test_str(self):
+        test_title = Trip(title='A trip')
+        self.assertEqual(str(test_title), 'A trip')
