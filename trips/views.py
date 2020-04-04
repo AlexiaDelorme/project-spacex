355d70c599dbe1c43ed1aebb125478b795c587ee
Alexia Delorme <alexia.delorme@gmail.com>
2020-04-04 17:20:17 +0000

    Manually created dict to handle trip selection

--- trips/views.py
+++ trips/views.py
@@ -29,11 +29,25 @@ def trip_detail_page(request, pk):
     # trip_dict = serializers.serialize('json', Trip.objects.all())
     trip_dict = model_to_dict(trip)
     model_obj_dep = trip_dict["departure"]
-    departures = serializers.serialize('json', model_obj_dep, fields=('site_name', 'date'))
+    # departures = serializers.serialize('json', model_obj_dep, fields=('site_name', 'date'))
+    departures = [
+        {'id': '0',
+         'site_name': 'Kourou',
+         'country': 'French Guyana',
+         'date': ['15-04-2020', '15-05-2020', '15-06-2020']
+         },
+        {'id': '1',
+         'site_name': 'Cap Canaveral',
+         'country': 'United States',
+         'date': ['01-04-2020', '01-05-2020', '01-06-2020']
+         }
+    ]
+    string_departures = json.dumps(departures)
     context = {
         "page_title": "Detail",
         "trip": trip,
         "slot": range(slot+1),
-        "departures": departures
+        "departures": departures,
+        "string_departures": string_departures
     }
     return render(request, "trip_detail.html", context)
