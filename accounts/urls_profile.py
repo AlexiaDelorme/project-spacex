017f065ae735cd69a9202c83e5f3b2ce11689b01
Alexia Delorme <alexia.delorme@gmail.com>
2020-04-05 15:16:43 +0000

    Moved profile inside urls_profile, harmonized urls and created urls for edit passenger

--- accounts/urls_profile.py
+++ accounts/urls_profile.py
@@ -1,22 +1,29 @@
 from django.urls import path
 from .views import (
+    profile_page,
     create_passenger_details_page,
     create_contact_details_page,
+    edit_passenger_details_page,
     edit_contact_details_page
 )
 
 
 urlpatterns = [
+    path('', profile_page, name='profile'),
     path(
-        'create/passenger/',
+        'create-passenger/',
         create_passenger_details_page,
         name='create_passenger_details'),
     path(
-        'create/contact/',
+        'create-contact/',
         create_contact_details_page,
         name='create_contact_details'),
     path(
-        'edit/contact/',
+        'edit-passenger/',
+        edit_passenger_details_page,
+        name='edit_passenger_details'),
+    path(
+        'edit-contact/',
         edit_contact_details_page,
         name='edit_contact_details'),
 ]
