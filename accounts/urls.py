3a0e39a999dc38ed51279d4425f8c28a34042f4d
Alexia Delorme <alexia.delorme@gmail.com>
2020-04-05 15:14:26 +0000

    Removed profile url and moved inside urls_profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,16 +2,14 @@ from django.urls import path, include
 from .views import (
     logout_page,
     login_page,
-    signup_page,
-    profile_page
-    )
+    signup_page
+)
 
 
 urlpatterns = [
     path('logout/', logout_page, name='logout'),
     path('login/', login_page, name='login'),
     path('signup/', signup_page, name='signup'),
-    path('profile/', profile_page, name='profile'),
-    path('details/', include('accounts.urls_profile')),
+    path('profile/', include('accounts.urls_profile')),
     path('password-reset/', include('accounts.urls_reset'))
 ]
