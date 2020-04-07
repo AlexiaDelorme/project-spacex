4c18d8940cc78bad78f4af81992b675f75a23207
Alexia Delorme <alexia.delorme@gmail.com>
2020-04-07 21:27:07 +0000

    Modified trip detail view to reflect last model changes

--- trips/views.py
+++ trips/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 from django.core import serializers
 from django.forms.models import model_to_dict
-from .models import Trip, TripCategory
+from .models import Trip, TripCategory, DepartureSite
 import json
 
 
@@ -23,31 +23,13 @@ def trips_page(request):
 
 
 def trip_detail_page(request, pk):
-    trip = get_object_or_404(Trip, pk=pk)
-    slot = trip.slot
-    # Create a string dic to be passed to the dom
-    # trip_dict = serializers.serialize('json', Trip.objects.all())
-    # model_obj_dep = trip_dict["departure"]
-    # trip_dict = model_to_dict(trip)
-    # departures = serializers.serialize('json', model_obj_dep, fields=('site_name', 'date'))
-    departures = [
-        {'id': '0',
-         'site_name': 'Kourou (CSG)',
-         'country': 'French Guyana',
-         'date': ['15-04-2020', '15-05-2020', '15-06-2020']
-         },
-        {'id': '1',
-         'site_name': 'Cap Canaveral',
-         'country': 'United States',
-         'date': ['01-05-2020', '01-06-2020', '01-07-2020']
-         }
-    ]
-    string_departures = json.dumps(departures)
+    trip_category = get_object_or_404(TripCategory, pk=pk)
+    trips = Trip.objects.all().filter(category=trip_category)
+    departure_sites = DepartureSite.objects.all()
     context = {
         "page_title": "Detail",
-        "trip": trip,
-        "slot": range(slot+1),
-        "departures": departures,
-        "string_departures": string_departures
+        "trip_category": trip_category,
+        "trips": trips,
+        "departure_sites": departure_sites
     }
     return render(request, "trip_detail.html", context)
