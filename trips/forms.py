afa332abdc1e51bb96f7c1310c2dc6fdbf5a1cde
Alexia Delorme <alexia.delorme@gmail.com>
2020-04-10 13:33:58 +0000

    Added customization to fields for TripSearchForm

--- trips/forms.py
+++ trips/forms.py
@@ -5,11 +5,24 @@ from .models import DepartureSite
 class TripSearchForm(forms.Form):
 
     departure_site = forms.ModelChoiceField(
+        label='<i class="fas fa-space-shuttle"></i> Departure Site',
         queryset=DepartureSite.objects.all(),
         empty_label="Choose site"
     )
-    departure_date = forms.DateField()
-    passenger_number = forms.IntegerField(max_value=5, min_value=1)
+    departure_date = forms.DateField(
+        label='<i class="fas fa-calendar-day"></i> Departure Date',
+        widget=forms.TextInput(
+            attrs={
+                'id': 'datepicker',
+                'class': 'bg-white'
+            }
+        )
+    )
+    passenger_number = forms.IntegerField(
+        label='<i class="fas fa-users"></i> Passenger(s)',
+        max_value=5,
+        min_value=1
+    )
 
     class Meta:
         fields = ['departure_site', 'departure_date', 'passenger_number']
