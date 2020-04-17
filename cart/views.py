ace39ee0236332d5cd25ce07ee50514bd0eb27a6
Alexia Delorme <alexia.delorme@gmail.com>
2020-04-17 16:02:31 +0000

    Corrected var name for passenger

--- cart/views.py
+++ cart/views.py
@@ -7,8 +7,8 @@ def view_cart(request):
     return render(request, "cart.html")
 
 
-def add_to_cart(request, id):
-    passenger = int(request.POST.get('passenger'))
+def add_to_cart(request, id, passenger):
+    passenger = int(request.POST.get('passenger_number'))
 
     cart = request.session.get('cart', {})
     if id in cart:
@@ -17,11 +17,11 @@ def add_to_cart(request, id):
         cart[id] = cart.get(id, passenger)
 
     request.session['cart'] = cart
-    return redirect(reverse('home'))
+    return redirect(reverse('view_cart'))
 
 
 def adjust_cart(request, id):
-    passenger = int(request.POST.get('passenger'))
+    passenger = int(request.POST.get('passenger_number'))
     cart = request.session.get('cart', {})
 
     if passenger > 0:
