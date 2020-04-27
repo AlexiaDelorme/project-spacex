8ae0ee8997027291a1b7dcf6dcdc4b2f60a3ebbb
Alexia Delorme <alexia.delorme@gmail.com>
2020-04-27 17:03:21 +0000

    Added script to remove item using ajax but csrf token issue

--- static/js/main.js
+++ static/js/main.js
@@ -17,19 +17,37 @@ $(document).ready(function () {
             sum += parseFloat($(this).text().replace(/,/g, ''));
         });
         $("#totalCartPrice").text(sum.toLocaleString());
+
         // itemID is an empty <span> of the cart.html file to get the url-for item.id
+        // var csrfToken = '{{ csrf_token }}';
         // var itemID =  $(this).parents("form").children("span[id^=item-id-]").attr("id").replace("item-id-", "");
-        // $.post(`/cart/adjust/${itemID}/`, {passenger: inputPassenger}); // ajax call without refreshing page
+        // $.post(`/cart/adjust/${itemID}/`, {'csrfmiddlewaretoken': csrfToken, 'passenger': inputPassenger}); // ajax call without refreshing page
+
+        // Adjust quantity on input change
         var form = $(this).parents("form");
         form.submit();
+
     });
-    
+
+    // Remove item and reload on click
+    $('.remove-item').click(function () {
+        var csrfToken = '{{ csrf_token }}';
+        var itemId = $(this).attr('id').split('remove_')[1];
+        var url = `/cart/remove/${itemId}/`;
+        var data = { 'csrfmiddlewaretoken': csrfToken };
+
+        $.post(url, data)
+            .done(function () {
+                location.reload();
+            });
+    })
+
 
     // Set up datepicker
-	$("#datepicker").datepicker({
+    $("#datepicker").datepicker({
         autoclose: true,
         todayHighlight: true,
         format: 'yyyy-mm-dd'
-	});
+    });
 
 });
