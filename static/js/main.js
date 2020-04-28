bce10f814e788f16a6b83e56b8ae3bce36396b45
Alexia Delorme <alexia.delorme@gmail.com>
2020-04-28 11:34:31 +0000

    Removed script for remove_cart, adjust cart with ajax, loaded csrf token

--- static/js/main.js
+++ static/js/main.js
@@ -18,30 +18,17 @@ $(document).ready(function () {
         });
         $("#totalCartPrice").text(sum.toLocaleString());
 
-        // itemID is an empty <span> of the cart.html file to get the url-for item.id
-        // var csrfToken = '{{ csrf_token }}';
-        // var itemID =  $(this).parents("form").children("span[id^=item-id-]").attr("id").replace("item-id-", "");
-        // $.post(`/cart/adjust/${itemID}/`, {'csrfmiddlewaretoken': csrfToken, 'passenger': inputPassenger}); // ajax call without refreshing page
+        // Update cart without reloading the page
+        var itemId = $(this).siblings(".tripPrice").attr("id").split("remove_")[1];
+        var url = `/cart/adjust/${itemId}/`;
+        var data = { 'csrfmiddlewaretoken': csrfToken, 'passenger': inputPassenger };
+        $.post(url, data);
 
-        // Adjust quantity on input change
-        var form = $(this).parents("form");
-        form.submit();
+        // Submit form on input change
+        // var form = $(this).parents("form");
+        // form.submit();
 
     });
-
-    // Remove item and reload on click
-    $('.remove-item').click(function () {
-        var csrfToken = '{{ csrf_token }}';
-        var itemId = $(this).attr('id').split('remove_')[1];
-        var url = `/cart/remove/${itemId}/`;
-        var data = { 'csrfmiddlewaretoken': csrfToken };
-
-        $.post(url, data)
-            .done(function () {
-                location.reload();
-            });
-    })
-
 
     // Set up datepicker
     $("#datepicker").datepicker({
