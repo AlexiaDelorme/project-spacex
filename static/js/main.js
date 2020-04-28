3bcfd029415a112d1e2510e2dca8df2e17cab612
Alexia Delorme <alexia.delorme@gmail.com>
2020-04-28 14:33:49 +0000

    Add script to update cart item number displayed in navbar

--- static/js/main.js
+++ static/js/main.js
@@ -8,25 +8,35 @@ $(document).ready(function () {
 
     // Update total price per trip according to passenger number  
     $(".passengerAdjust").change(function () {
+
         var inputPassenger = $(this).val();
         var tripPrice = $(this).siblings(".tripPrice").val();
+
+        // Update total price for each cart item
         var totalTripPrice = (inputPassenger * tripPrice).toLocaleString();
         $(this).parents("form").siblings(".d-inline-flex").children(".totalPrice").text(totalTripPrice);
+
+        // Update amount for total cart
         var sum = 0;
         $(".totalPrice").each(function () {
             sum += parseFloat($(this).text().replace(/,/g, ''));
         });
         $("#totalCartPrice").text(sum.toLocaleString());
 
-        // Update cart without reloading the page
+        // Update number of cart item in navbar
+        var cartItem = 0;
+        $(".passengerAdjust").each(function () {
+            cartItem += parseFloat($(this).val());
+        });
+        if (cartItem > 0) {
+            $("#cart-label").text(cartItem);
+        }
+
+        // Update cart content without reloading the page
         var itemId = $(this).siblings(".tripPrice").attr("id").split("remove_")[1];
         var url = `/cart/adjust/${itemId}/`;
         var data = { 'csrfmiddlewaretoken': csrfToken, 'passenger': inputPassenger };
         $.post(url, data);
-
-        // Submit form on input change
-        // var form = $(this).parents("form");
-        // form.submit();
 
     });
 
