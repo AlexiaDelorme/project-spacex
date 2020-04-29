9450164a79d0989438bb34dafe73a1410782a23a
Alexia Delorme <alexia.delorme@gmail.com>
2020-04-29 15:10:14 +0000

    Set up passenger details form and recap booking for checkout

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django_countries.fields import CountryField
 
 # Create your models here.
+class OtherPassenger(models.Model):
+
+    CHOICES_TITLE = [
+        ('Mr', 'Mr'), ('Mrs', 'Mrs'), ('Miss', 'Miss'), ('Ms', 'Ms')
+    ]
+    CHOICES_MONTH = [('January', 'January'),
+                     ('February', 'February'),
+                     ('March', 'March'),
+                     ('April', 'April'),
+                     ('May', 'May'),
+                     ('June', 'June'),
+                     ('July', 'July'),
+                     ('August', 'August'),
+                     ('September', 'September'),
+                     ('October', 'October'),
+                     ('November', 'November'),
+                     ('December', 'December')
+                     ]
+
+    title = models.CharField(max_length=10, choices=CHOICES_TITLE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_month = models.CharField(max_length=20, choices=CHOICES_MONTH)
+    birth_day = models.IntegerField()
+    birth_year = models.IntegerField()
+    citizenship = CountryField()
+    passport_id = models.CharField(max_length=30)
+
+    @property
+    def birth_date(self):
+        return f"{self.birth_month} {self.birth_day}, {self.birth_year}"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.models import User
+from accounts.models import Passenger
+from accounts.forms import UserPassengerForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
 
 @login_required
 def checkout_passengers_page(request):
     """Render page to provide passengers details"""
 
-    return render(request, "checkout_passengers.html")
+    user = User.objects.get(email=request.user.email)
+
+    try:
+        passenger = Passenger.objects.get(user=request.user)
+    except Passenger.DoesNotExist:
+        passenger = None
+
+    if passenger is not None:
+        form = UserPassengerForm(instance=request.user.passenger)
+    else:
+        form = UserPassengerForm()
+
+    context = {
+        "page_title": "Passenger details",
+        "user": user,
+        "form": form
+    }
+
+    return render(request, "checkout_passengers.html", context)
