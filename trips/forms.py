b732218f043faafc2e03322729d51b65264ef0bd
Alexia Delorme <alexia.delorme@gmail.com>
2020-05-06 14:04:16 +0000

    Created a trip search for all trips view

--- trips/forms.py
+++ trips/forms.py
@@ -1,5 +1,40 @@
 from django import forms
-from .models import DepartureSite
+from .models import DepartureSite, TripCategory
+
+
+class AllTripSearchForm(forms.Form):
+
+    destination = forms.ModelChoiceField(
+        queryset=TripCategory.objects.all(),
+        empty_label="Choose destination"
+    )
+    departure_site = forms.ModelChoiceField(
+        label='<i class="fas fa-space-shuttle"></i> Departure Site',
+        queryset=DepartureSite.objects.all(),
+        empty_label="Choose site"
+    )
+    departure_date = forms.DateField(
+        label='<i class="fas fa-calendar-day"></i> Departure Date',
+        widget=forms.TextInput(
+            attrs={
+                'id': 'datepicker',
+                'class': 'bg-white'
+            }
+        )
+    )
+    passenger_number = forms.IntegerField(
+        label='<i class="fas fa-users"></i> Passenger(s)',
+        max_value=5,
+        min_value=1
+    )
+
+    class Meta:
+        fields = [
+            'destination',
+            'departure_site',
+            'departure_date',
+            'passenger_number'
+        ]
 
 
 class TripSearchForm(forms.Form):
