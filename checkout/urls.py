8aeef059df0e6d51d4bb4c92b562233318889ce1
Alexia Delorme <alexia.delorme@gmail.com>
2020-05-08 13:26:55 +0000

    Created url for save_passenger_to_cart view

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import checkout_confirm_page, checkout_passengers_page
+from .views import checkout_confirm_page, checkout_passengers_page, save_passenger_to_cart
 
 
 urlpatterns = [
@@ -12,5 +12,10 @@ urlpatterns = [
         'passengers/',
         checkout_passengers_page,
         name='checkout_passengers'
+    ),
+    path(
+        'save-passenger/<int:id>/',
+        save_passenger_to_cart,
+        name='save_passenger'
     ),
 ]
