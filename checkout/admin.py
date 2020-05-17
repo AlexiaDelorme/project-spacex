70875e2256daf5b26f5b2f0f5723a3e67b9d0b5a
Alexia Delorme <alexia.delorme@gmail.com>
2020-05-17 08:50:57 +0000

    Regitered BookingReference model to admin site

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import OtherPassenger
+from .models import OtherPassenger, BookingReference
 
 # Register your models here.
 admin.site.register(OtherPassenger)
+admin.site.register(BookingReference)
