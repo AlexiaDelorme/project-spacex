dc0a806d503bbf110a13532cd504248fa5fc766e
Alexia Delorme <alexia.delorme@gmail.com>
2020-06-03 15:25:40 +0000

    Customized admin view for models

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 from .models import OtherPassenger, BookingReference
 
+
+class OtherPassengerAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birth_date',
+        'citizenship',
+        'passport_id',
+    )
+
+    ordering = ('last_name',)
+
+
+class BookingReferenceAdmin(admin.ModelAdmin):
+    list_display = (
+        'trip',
+        'booker',
+        'confirmation_status',
+    )
+
+    ordering = ('trip',)
+
+
 # Register your models here.
-admin.site.register(OtherPassenger)
-admin.site.register(BookingReference)
+admin.site.register(OtherPassenger, OtherPassengerAdmin)
+admin.site.register(BookingReference, BookingReferenceAdmin)
--- trips/admin.py
+++ trips/admin.py
@@ -1,22 +1,41 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
-from .models import (
-    TripCategory,
-    DepartureSite,
-    TripImage,
-    RequiredDocument,
-    Trip
-)
+from . import models
+
+
+class DepartureSiteAdmin(admin.ModelAdmin):
+    list_display = (
+        'site_name',
+        'country',
+        'site_code'
+    )
+
+    ordering = ('country',)
+
+
+class TripCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'destination',
+        'price',
+        'destination_code'
+    )
+
+    ordering = ('destination',)
+
+
+class TripAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'departure_date',
+        'return_date',
+        'slot',
+    )
+
+    ordering = ('departure_date',)
+
 
 # Register your models here.
-admin.site.register(DepartureSite)
-
-admin.site.register(RequiredDocument)
-
-admin.site.register(TripImage)
-
-admin.site.register(TripCategory)
-
-admin.site.register(Trip)
+admin.site.register(models.DepartureSite, DepartureSiteAdmin)
+admin.site.register(models.RequiredDocument)
+admin.site.register(models.TripImage)
+admin.site.register(models.TripCategory, TripCategoryAdmin)
+admin.site.register(models.Trip, TripAdmin)
