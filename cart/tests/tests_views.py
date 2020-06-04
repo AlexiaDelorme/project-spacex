dc308caed431234dcdab40ee96f0c3ae8c815ee7
Alexia Delorme <alexia.delorme@gmail.com>
2020-06-04 17:26:14 +0000

    Wrapped comments for tests

--- cart/tests/tests_views.py
+++ cart/tests/tests_views.py
@@ -14,21 +14,23 @@ class TestCartViewPage(TestCase):
     def test_get_cart_page(self):
         response = self.client.get('/cart/')
         session = self.client.session
-        
+
         self.assertEqual(response.status_code, 200)
         self.assertNotIn('booking_references', session, msg=None)
         self.assertTemplateUsed(response, 'cart.html')
 
     def test_cart_page_session_logged_out(self):
-        """Test that session variable 'referrer' is equal to checkout when user is logged out."""
+        """Test that session variable 'referrer' is
+        equal to checkout when user is logged out."""
 
         self.client.get('/cart/')
         session = self.client.session
 
         self.assertEqual(session['referrer'], 'checkout')
-    
+
     def test_cart_page_session_logged_in(self):
-        """Test that session variable 'referrer' is empty when user is logged in."""
+        """Test that session variable 'referrer' is
+        empty when user is logged in."""
 
         # Log in dummy user
         client = Client()
@@ -40,4 +42,3 @@ class TestCartViewPage(TestCase):
         self.assertTrue(logged_in)
         # Test block failing
         # self.assertNotIn('referrer', session, msg=None)
-    
