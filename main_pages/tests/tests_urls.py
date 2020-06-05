343b62aaf7341048d35202e0bb0c6fd9bf490fb8
Alexia Delorme <alexia.delorme@gmail.com>
2020-06-05 15:49:57 +0000

    Formatted python file

--- main_pages/tests/tests_urls.py
+++ main_pages/tests/tests_urls.py
@@ -1,6 +1,11 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from main_pages.views import home_page, contact_page, about_page, scientists_page
+from main_pages.views import (
+    home_page,
+    contact_page,
+    about_page,
+    scientists_page
+)
 
 
 class TestUrls(SimpleTestCase):
@@ -16,7 +21,7 @@ class TestUrls(SimpleTestCase):
     def test_contact_url_resolves(self):
         url = reverse('contact')
         self.assertEquals(resolve(url).func, contact_page)
-    
+
     def test_scientists_url_resolves(self):
         url = reverse('scientists')
         self.assertEquals(resolve(url).func, scientists_page)
