06ebedac834cfafa5a9ef793cf5397fda3d78beb
Alexia Delorme <alexia.delorme@gmail.com>
2020-06-18 14:00:09 +0000

    Reorganized tests for main pages app views

--- main_pages/tests/tests_views.py
+++ main_pages/tests/tests_views.py
@@ -5,22 +5,30 @@ from main_pages.forms import ContactForm
 class TestMainPageViews(TestCase):
 
     def test_get_home_page(self):
+
         response = self.client.get('/')
+
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
 
     def test_get_about_page(self):
+
         response = self.client.get('/main/about/')
+
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'about.html')
 
     def test_get_contact_page(self):
-        # Testing get request
+
         response = self.client.get('/main/contact/')
         contact_form = response.context['form']
+
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'contact.html')
         self.assertEqual(type(contact_form), ContactForm)
+
+    def test_post_contact_page_success(self):
+
         # Testing successful post request
         post_form = {
             'subject': 'Information Request',
@@ -30,7 +38,11 @@ class TestMainPageViews(TestCase):
             'message': 'This is a test message'
         }
         post_response = self.client.post('/main/contact/', post_form)
+
         self.assertRedirects(post_response, '/main/contact/')
+
+    def test_post_contact_page_failure(self):
+
         # Testing failed post request
         post_form = {
             'subject': 'Information Request',
@@ -38,13 +50,16 @@ class TestMainPageViews(TestCase):
         }
         post_response = self.client.post('/main/contact/', post_form)
         messages = list(post_response.context['messages'])
+
         self.assertEqual(len(messages), 1)
         self.assertEqual(
             str(messages[0]), 'Sorry, we were enable to send your request')
 
     def test_get_scientists_page(self):
+
         response = self.client.get('/main/scientists/')
         context = response.context['page_title']
+
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'scientists.html')
         self.assertEqual(context, 'Scientific services')
