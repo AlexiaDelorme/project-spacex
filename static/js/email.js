934235d89feb105e33d8e1762832ce84cb4bc567
Alexia Delorme <alexia.delorme@gmail.com>
2020-07-22 20:13:30 +0000

    Added sweet alerts for contact form feedback

--- static/js/email.js
+++ static/js/email.js
@@ -1,20 +1,41 @@
 (function () {
-    emailjs.init(emailjsUser);
+  emailjs.init(emailjsUser);
 })();
 
 function sendMail(contactForm) {
-    emailjs.send("gmail", "spacex", {
-        "from_name": contactForm.first.value + " " + contactForm.last.value,
-        "from_email": contactForm.email.value,
-        "contact_request": contactForm.message.value,
-        "subject": contactForm.subject.value
+  emailjs
+    .send("gmail", "spacex", {
+      from_name: contactForm.first.value + " " + contactForm.last.value,
+      from_email: contactForm.email.value,
+      contact_request: contactForm.message.value,
+      subject: contactForm.subject.value,
     })
-    .then(
-        function(response) {
-            alert(response);
-        },
-        function(error) {
-            alert(error);
-        }
-    );
+    .then(function(response) {
+        contact_request_success();
+        console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+        contact_request_fail();
+        console.log('FAILED...', error);
+    });
+}
+
+function contact_request_success() {
+  console.log("The contact_request_success is being called");
+  swal({
+    title: "Request sent!",
+    text: "Your message was successfully sent to SpaceX.",
+    icon: "success",
+    button: "ok",
+  });
+}
+
+function contact_request_fail() {
+  console.log("The contact_request_fail is being called");
+  swal({
+    title: "Request not sent!",
+    text:
+      "Sorry, there was an issue with the server when sending your request.",
+    icon: "warning",
+    button: "ok",
+  });
 }
