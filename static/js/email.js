a69ee6c12b13af24f68d23a7b43fc17bdd991cf5
Alexia Delorme <alexia.delorme@gmail.com>
2020-07-23 10:24:07 +0000

    Added ajax call for emailJS but issue with value

--- static/js/email.js
+++ static/js/email.js
@@ -2,40 +2,57 @@
   emailjs.init(emailjsUser);
 })();
 
-function sendMail(contactForm) {
-  emailjs
-    .send("gmail", "spacex", {
+const contactForm = $("#contact-form");
+
+$("#contact-form").submit(function (e) {
+  e.preventDefault();
+
+  var data = {
+    service_id: "gmail",
+    template_id: "spacex",
+    user_id: emailjsUser,
+    template_params: {
       from_name: contactForm.first.value + " " + contactForm.last.value,
       from_email: contactForm.email.value,
       contact_request: contactForm.message.value,
       subject: contactForm.subject.value,
+    },
+  };
+
+  $.ajax("https://api.emailjs.com/api/v1.0/email/send", {
+    type: "POST",
+    data: JSON.stringify(data),
+    contentType: "application/json",
+  })
+    .done(function () {
+      contact_request_success();
+      console.log("Your mail is sent!");
     })
-    .then(function(response) {
-        contact_request_success();
-        console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-        contact_request_fail();
-        console.log('FAILED...', error);
+    .fail(function (error) {
+      contact_request_fail();
+      console.log("Oops... " + JSON.stringify(error));
     });
-}
+});
 
 function contact_request_success() {
-  console.log("The contact_request_success is being called");
   swal({
-    title: "Request sent!",
-    text: "Your message was successfully sent to SpaceX.",
+    title: "Message sent!",
+    text: "Thank you, your message request was successfully sent to SpaceX.",
     icon: "success",
-    button: "ok",
+    confirmButtonText: 'ok',
+    confirmButtonColor: '#64a19d',
+  }).then(() => {
+    location.reload();
   });
 }
 
 function contact_request_fail() {
-  console.log("The contact_request_fail is being called");
   swal({
-    title: "Request not sent!",
+    title: "Message not sent!",
     text:
       "Sorry, there was an issue with the server when sending your request.",
     icon: "warning",
-    button: "ok",
+    buttons: false,
+    timer: 5000,
   });
 }
