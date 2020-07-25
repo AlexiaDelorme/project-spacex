afddbce7e42c358c20e7b5cc84d17c0f841860a2
Alexia Delorme <alexia.delorme@gmail.com>
2020-07-25 18:02:52 +0000

    Amended col size of radio titles for xs devices

--- static/js/main.js
+++ static/js/main.js
@@ -118,7 +118,7 @@ $(document).ready(function () {
     */
     var parentWrapper = $(".radio-title").children("#div_id_title").children("div");
     $(parentWrapper).addClass("row text-left ml-2");
-    $(parentWrapper).children(".form-check").addClass("col-2");
+    $(parentWrapper).children(".form-check").addClass("col-3 col-sm-2");
 
     /*
         Set up datepicker for trip search form
