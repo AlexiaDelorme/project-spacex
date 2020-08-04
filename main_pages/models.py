3babf87fb27d15a3966c7384d96491344531bb90
Alexia Delorme <alexia.delorme@gmail.com>
2020-08-04 18:24:08 +0000

    Added model for passenger testimonial

--- main_pages/models.py
+++ main_pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Testimonial(models.Model):
+    passenger = models.ForeignKey(User, on_delete=models.PROTECT)
+    text = models.TextField()
+    trip_date = models.DateField()
+    image = models.ImageField(blank=True, upload_to='testimonial_pics')
+
+    class Meta:
+        ordering = ['trip_date']
+
+    def __str__(self):
+        f"{self.passenger} - trip {self.trip_date}"
